39b18c9434cd51667d1210c5603205f1a5994b80
Robin Ramael <robin.ramael@gmail.com>
2020-06-17 17:07:19 +0200

    version bump to 1.0.1 because of readme weirdness

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/robinramael/prepared_properties",
-    version="1.0.0",
+    version="1.0.1",
     zip_safe=False,
 )
